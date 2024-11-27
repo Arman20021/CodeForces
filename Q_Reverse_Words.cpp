70e98f59facbf94838f3b82b7900877677a79f5b
Arman <156702166+Arman20021@users.noreply.github.com>
2024-11-27 16:04:07 +0600

    Add files via upload

---
+++ Q_Reverse_Words.cpp
@@ -0,0 +1,20 @@
+ #include <bits/stdc++.h>
+ using namespace std;
+ int main()
+ {
+  
+string a;
+getline(cin,a);
+stringstream ss(a);
+string word;
+ss>>word;
+reverse(word.begin(),word.end());
+cout<<word;
+while(ss>>word)
+{
+    reverse(word.begin(),word.end());
+    cout<<" "<<word;
+
+}
+     return 0;
+ }
